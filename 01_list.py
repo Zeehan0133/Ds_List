b641c162d44615f802ff20abd30da92d2efe6970
Zeehan0133 <zyne0133@gmail.com>
2021-11-27 23:55:10 +0530

    [Zeehan] Add:sum all items from  list

---
+++ 01_list.py
@@ -0,0 +1,18 @@
+"""
+@Author: Zeehan 
+@Date: 2021-26-11 19:00:30
+@Title :  program to sum all the items in a list.
+"""
+"""
+Description: create a list iterate the for loop and then print the result
+           
+
+Parameter:
+      
+Return:
+"""
+mylist=[5,10,5,20,10]
+result=0
+for i in mylist:
+    result=result + i
+print("sum of all the element is ",result)
