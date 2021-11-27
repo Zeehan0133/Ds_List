29de9497fb02b4b365c45cbf50322be488281c72
Zeehan0133 <zyne0133@gmail.com>
2021-11-28 00:03:31 +0530

    [Zeehan] Add:remove duplicates from list

---
+++ 06_list.py
@@ -0,0 +1,16 @@
+"""
+@Author: Zeehan 
+@Date: 2021-26-11 19:00:30
+@Title :  remove duplicates from the list 
+"""
+"""
+Description: To remove the duplicates from a list, you can make use of the built-in function set(). 
+The specialty of set() method is that it returns distinct elements.
+
+Parameter:
+      
+Return:
+"""
+mylist=[1,1,1,2,2,3,4,5,5,6,7,8,7]
+result=set(mylist)
+print("remove duplicates ", result)
