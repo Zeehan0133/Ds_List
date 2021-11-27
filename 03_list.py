aa1cc105b39b4e6114817891a91410fba25950f8
Zeehan0133 <zyne0133@gmail.com>
2021-11-27 23:58:12 +0530

    [Zeehan] Add:print smallest number from   list

---
+++ 03_list.py
@@ -0,0 +1,15 @@
+"""
+@Author: Zeehan 
+@Date: 2021-26-11 19:00:30
+@Title : program to print smallest number from the list
+"""
+"""
+Description:
+          create a list and find minimum number with the help of min method
+
+Parameter:
+      
+Return:
+"""
+mylist=[10,20,1,99,12]
+print("smallest number is :", min(mylist))
