33e25e9b72862b368984307a414440696ddd8b57
Zeehan0133 <zyne0133@gmail.com>
2021-11-28 00:10:32 +0530

    [Zeehan] Add:append list

---
+++ 13_list.py
@@ -0,0 +1,17 @@
+"""
+@Author: Zeehan 
+@Date: 2021-26-11 19:00:30
+@Title :  program to append a list to the second list.
+"""
+"""
+Description: append the list with the help of append method
+           
+
+Parameter:
+      
+Return:
+"""
+list1=[1,2,3,4]
+list2=[5,6,7,8]
+list1.extend(list2)
+print("appended list is :",list1)
