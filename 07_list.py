93536a6e4a9cc09889fad2500b015508e4789282
Zeehan0133 <zyne0133@gmail.com>
2021-11-28 00:04:27 +0530

    [Zeehan] Add:program to copy list

---
+++ 07_list.py
@@ -0,0 +1,17 @@
+"""
+@Author: Zeehan 
+@Date: 2021-26-11 19:00:30
+@Title :  program to  copy a list.
+"""
+"""
+Description: create a list and copy the list with the help of copy() method
+           
+
+Parameter:
+      
+Return:
+"""
+mylist=[1,2,3,4,5,6]
+print("original list :", mylist)
+newlist=mylist.copy()
+print("copy  list :", newlist)
