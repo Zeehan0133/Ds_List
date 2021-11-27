830efe9a8e16566a018c2013b9418141f2b56dd2
Zeehan0133 <zyne0133@gmail.com>
2021-11-27 23:56:42 +0530

    [Zeehan] Add:multiply all items from  list

---
+++ 02_list.py
@@ -0,0 +1,18 @@
+"""
+@Author: Zeehan 
+@Date: 2021-26-11 19:00:30
+@Title : program to multiplies all the items in a list.
+"""
+"""
+Description:
+          create a list iterate the for loop and then print the result
+
+Parameter:
+      
+Return:
+"""
+mylist=[1,2,3]
+result=1
+for i in mylist:
+    result=result*i
+print("multiplication of all the elements is :" , result)
