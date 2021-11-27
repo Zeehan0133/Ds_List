2a49df2801e4b4ec46452a610155d3066eb13db5
Zeehan0133 <zyne0133@gmail.com>
2021-11-28 00:08:15 +0530

    [Zeehan] Add:generate permutation in list

---
+++ 11_list.py
@@ -0,0 +1,16 @@
+"""
+@Author: Zeehan 
+@Date: 2021-26-11 19:00:30
+@Title :  program generate permutation in  a list.
+"""
+"""
+Description: i imported one module that  Itertool is a module that provides various functions 
+that work on iterators to produce complex iterators
+           
+
+Parameter:
+      
+Return:
+"""
+import itertools
+print(list(itertools.permutations([1,2,3])))
